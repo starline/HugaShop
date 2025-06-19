07e6c603c19444492d01018acc6811fba5edc299
Andri Huga <aa.guzhva@gmail.com>
2025-06-19 17:09:44 -0600

    р

--- src/Controller/Admin/Settings/BackupController.php
+++ src/Controller/Admin/Settings/BackupController.php
@@ -19,11 +19,11 @@ use PhpZip\ZipFile;
 use HugaShop\Api\Config;
 use HugaShop\Api\Design;
 use HugaShop\Api\Request;
-use HugaShop\Api\Database;
 use HugaShop\Api\Settings;
 use App\Controller\BaseAdminController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class BackupController extends BaseAdminController
 {
@@ -55,7 +55,7 @@ class BackupController extends BaseAdminController
 
                         ### Дамп базы
                         // Ложим базу в папку чтобы затем добавить файл в архив
-                        Database::dump($backup_dir . Config::get('database')->name . ".sql");
+                        $this->dump($backup_dir . Config::get('database')->name . ".sql");
                         chmod($backup_dir . Config::get('database')->name . ".sql", 0777);
 
                         ### Архивируем
@@ -76,7 +76,6 @@ class BackupController extends BaseAdminController
 
                             $zipFile->addFile($backup_dir . Config::get('database')->name . ".sql");
                             $zipFile->saveAsFile($file_path);
-
                         } catch (\PhpZip\Exception\ZipException $e) {
                             trigger_error('Не могу заархивировать ' . $e);
                         } finally {
@@ -99,8 +98,7 @@ class BackupController extends BaseAdminController
                             $this->clean_dir($archive_dir);
                             $zipFile->extractTo($archive_dir);
 
-                            Database::restore($archive_dir . Config::get('database')->name . ".sql");
-
+                            $this->restore($archive_dir . Config::get('database')->name . ".sql");
                         } catch (\PhpZip\Exception\ZipException $e) {
                             trigger_error('Не могу разархивировать ' . $e);
                         } finally {
@@ -167,5 +165,136 @@ class BackupController extends BaseAdminController
         }
 
         closedir($handle);
+    }
+
+
+    /**
+     * Создаем бэкап всех таблиц базы данных.
+     * Сохраняем в вайл
+     * @param $filename
+     */
+    private function dump($filename)
+    {
+        $h = fopen($filename, 'w');
+
+        // Выбираем все таблицы
+
+        $result = Capsule::select("SHOW FULL TABLES LIKE '__%';");
+
+        foreach ($result as $row) {
+
+            // Имя таблицы — это свойство с динамическим ключом
+            $table_name = array_values((array) $row)[0];
+            $table_type = array_values((array) $row)[1];
+
+            if ($table_type == 'BASE TABLE') {
+                $this->dumpTable($table_name, $h);
+            }
+        }
+
+        fclose($h);
+    }
+
+
+    /**
+     * Dunmp of table
+     */
+    private function dumpTable($table, $h)
+    {
+
+        $result = Capsule::select("SELECT * FROM `$table`");
+
+        if ($result) {
+            fwrite($h, "/* Data for table $table */\n");
+            fwrite($h, "TRUNCATE TABLE `$table`;\n");
+
+            $num_rows = $result->num_rows;
+            $num_fields = $this->mysqli->field_count;
+
+            if ($num_rows > 0) {
+                $field_type = array();
+                $field_name = array();
+                $meta = $result->fetch_fields();
+
+                foreach ($meta as $m) {
+                    array_push($field_type, $m->type);
+                    array_push($field_name, $m->name);
+                }
+
+                $fields = join('`, `', $field_name);
+                fwrite($h, "INSERT INTO `$table` (`$fields`) VALUES\n");
+                $index = 0;
+
+                while ($row = $result->fetch_row()) {
+                    fwrite($h, "(");
+                    for ($i = 0; $i < $num_fields; $i++) {
+                        if (is_null($row[$i])) {
+                            fwrite($h, "null");
+                        } else {
+                            switch ($field_type[$i]) {
+                                case 'int':
+                                    fwrite($h, $row[$i]);
+                                    break;
+                                case 'string':
+                                case 'blob':
+                                default:
+                                    fwrite($h, "'" . $this->mysqli->real_escape_string($row[$i]) . "'");
+                            }
+                        }
+                        if ($i < $num_fields - 1) {
+                            fwrite($h, ",");
+                        }
+                    }
+                    fwrite($h, ")");
+
+                    if ($index < $num_rows - 1) {
+                        fwrite($h, ",");
+                    } else {
+                        fwrite($h, ";");
+                    }
+                    fwrite($h, "\n");
+
+                    $index++;
+                }
+            }
+        }
+        $result->free();
+        fwrite($h, "\n");
+    }
+
+
+    /**
+     * Восстанавливаем БД из файла
+     * @param $filename
+     */
+    private function restore($filename)
+    {
+        $templine = '';
+        $h = fopen($filename, 'r');
+
+        // Loop through each line
+        if ($h) {
+            while (!feof($h)) {
+                $line = fgets($h);
+
+                // Only continue if it's not a comment
+                if (substr($line, 0, 2) != '--' && $line != '') {
+
+                    // Add this line to the current segment
+                    $templine .= $line;
+
+                    // If it has a semicolon at the end, it's the end of the query
+                    if (substr(trim($line), -1, 1) == ';') {
+
+                        // Perform the query
+                        $this->mysqli->query($templine) or print('Error performing query \'<b>' . $templine . '</b>\': ' . $this->mysqli->error . '<br/><br/>');
+
+                        // Reset temp variable to empty
+                        $templine = '';
+                    }
+                }
+            }
+        }
+        fclose($h);
     }
 }
