25e1dec93b413c8795a4d48ddf190d81b5b5ec96
Andri Huga <aa.guzhva@gmail.com>
2025-07-03 18:03:15 -0600

    Move translate logic to OpenAI extension

--- src/Controller/Admin/Ajax/TranslateAjax.php
+++ src/Controller/Admin/Ajax/TranslateAjax.php
@@ -4,21 +4,13 @@
  * HugaShop - Sell anything
  *
  * @author Andri Huga
- * @version 1.2
+ * @version 1.3
  */
 
 namespace App\Controller\Admin\Ajax;
 
-use OpenAI;
-use HugaShop\Services\Config;
-use HugaShop\Services\Request;
-use App\Services\LanguageService;
-use HugaShop\Models\Product\Product;
+use HugaShop\Services\Extension;
 use App\Controller\BaseAdminController;
-use HugaShop\Models\Content\ContentPage;
-use HugaShop\Models\Content\ContentPost;
-use HugaShop\Models\Product\ProductBrand;
-use HugaShop\Models\Localization\Language;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -27,74 +19,12 @@ class TranslateAjax extends BaseAdminController
     #[Route('/admin/ajax/translate', name: 'TranslateAjaxAdmin')]
     public function translate()
     {
-        if (!Request::checkCSRF()) {
-            return new JsonResponse(['error' => 'csrf'], 400);
+        $Ext = Extension::makeExtension('OpenAI');
+
+        if (empty($Ext)) {
+            return new JsonResponse(['error' => 'extension'], 500);
         }
 
-        $entity    = Request::post('entity', 'string');
-        $id        = Request::postInt('id');
-        $lang_code = Request::post('lang', 'string');
-
-        if (empty($entity) || empty($id) || empty($lang_code)) {
-            return new JsonResponse(['error' => 'params'], 400);
-        }
-
-        $language = Language::getOne(['code' => $lang_code]);
-
-        if (empty($language)) {
-            return new JsonResponse(['error' => 'language'], 400);
-        }
-
-        if ($language->code == Language::getMain()->code) {
-            return new JsonResponse(['error' => 'is_main_language'], 400);
-        }
-
-        $model = null;
-        switch ($entity) {
-            case 'product':
-                $this->checkAdminAccess('product_content');
-                $model = Product::query()->find($id);
-                break;
-            case 'blog':
-                $this->checkAdminAccess('blog');
-                $model = ContentPost::query()->find($id);
-                break;
-            case 'page':
-                $this->checkAdminAccess('page');
-                $model = ContentPage::query()->find($id);
-                break;
-            case 'brand':
-                $this->checkAdminAccess('product_brand');
-                $model = ProductBrand::query()->find($id);
-                break;
-            default:
-                return new JsonResponse(['error' => 'entity'], 400);
-        }
-
-        if (empty($model)) {
-            return new JsonResponse(['error' => 'not_found'], 404);
-        }
-
-        $key = Config::get('openai')->key;
-        if (empty($key)) {
-            return new JsonResponse(['error' => 'openai_key'], 500);
-        }
-
-        $client = OpenAI::client($key);
-
-        $translated = [];
-        foreach ($model::getTranslatableFields() as $field) {
-            if (!empty($model->$field)) {
-                $result = $client->chat()->create([
-                    'model' => 'gpt-4o',
-                    'messages' => [
-                        ['role' => 'user', 'content' => 'Переведи на ' . $language->name . ': ' . $model->$field],
-                    ],
-                ]);
-                $translated[$field] = trim($result->choices[0]->message->content);
-            }
-        }
-
-        return new JsonResponse($translated);
+        return new JsonResponse($Ext->translate());
     }
 }
