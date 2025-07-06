a433da865146b465651c60f4ad4e61624eda5e9f
Andri Huga <aa.guzhva@gmail.com>
2025-07-05 18:32:13 -0600

    ...

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -6,7 +6,7 @@
  * @author Andri Huga
  * @version 2.5
  *
- * Домашняя страница сайта
+ * Home page
  *
  */
 
@@ -24,7 +24,6 @@ class MainController extends BaseFrontController
 {
 
     #[Route('/', name: 'Main', priority: 8)]
-    //#[Route('{_locale}', name: 'MainLocale', requirements: ['_locale' => 'ru|uk'], priority: -1)]
     public function main(): Response
     {
 
@@ -50,9 +49,9 @@ class MainController extends BaseFrontController
         }
 
         // Устанавливаем meta-теги
-        Design::assign('meta_title', Settings::getParam('company_name') . ' - ' . Settings::getParam('company_description'));
-        Design::assign('meta_description', Settings::getParam('company_name') . ' - ' . Settings::getParam('company_description'));
-        Design::assign('canonical', $this->generateUrl('Main'));
+        Design::assign('meta_title',        Settings::getParam('company_name') . ' - ' . Settings::getParam('company_description'));
+        Design::assign('meta_description',  Settings::getParam('company_name') . ' - ' . Settings::getParam('company_description'));
+        Design::assign('canonical',         $this->generateUrl('Main'));
         Design::assign('categories_products', $categories_products);
 
         return $this->fetchResponse('main.tpl');
