6d99b19bec36d5febbcb1f702d915e18efe1d2f4
Andri Huga <aa.guzhva@gmail.com>
2025-08-21 16:08:09 -0600

    feat: add tire service booking extension

---
+++ hugashop/crm/Extensions/BookingShinomontag/BookingShinomontag.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * HugaShop - Sell anything
+ *
+ * @author Andri Huga
+ * @version 1.0
+ */
+
+namespace HugaShop\Extensions\BookingShinomontag;
+
+use HugaShop\Extensions\BaseExtension;
+
+final class BookingShinomontag extends BaseExtension {}
