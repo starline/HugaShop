c9c7b3fbbc06a9af706c4db1c58ca099023c2b53
Andri Huga <aa.guzhva@gmail.com>
2025-08-26 18:37:32 -0600

    ImageResizeController

--- src/Controller/Front/ImageResizeController.php
+++ src/Controller/Front/ImageResizeController.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Сначало проверяетя есть ли уже файл на сервере через Ngix
+ * Сначала проверяется, есть ли уже файл на сервере через Nginx
  *
  * @author Andi Huga
- * @version 2.9
+ * @version 3.0
  *
  */
 
@@ -45,20 +45,21 @@ class ImageResizeController extends BaseController
         // Редиректить все разрешенные расширение на webp
         // Удалить 26.08.2026
         list($root_name, $ext, $width, $height, $set_watermark, $cut) = Image::getResizeParams($filename);
-        if (Image::isAllowedFormat($ext) and $ext != 'webp') {
+        if (Image::isAllowedFormat($ext) and $ext !== 'webp') {
 
             $flag = $set_watermark ? 'w' : '';
             $flag .= $cut ? 'c' : '';
 
             $image_url = Image::getImageURL($root_name, $width, $height, $flag, 'webp');
-            return $this->redirect($image_url, 301);
+            return $this->redirect($image_url, Response::HTTP_MOVED_PERMANENTLY);
         }
 
         $resized_filename = Image::resize($filename);
 
         if ($resized_filename and is_readable($resized_filename)) {
             $response = new Response(file_get_contents($resized_filename));
-            $response->headers->set('Content-type', 'image');
+            $mime_type = mime_content_type($resized_filename) ?: 'image';
+            $response->headers->set('Content-type', $mime_type);
             return $response;
         }
 
