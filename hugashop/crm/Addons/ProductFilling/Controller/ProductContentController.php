41cf579f41607f8e82266a196cf5dddb80dca1c8
Andri Huga <aa.guzhva@gmail.com>
2025-08-27 20:24:45 -0600

    Add product content editor

---
+++ hugashop/crm/Addons/ProductFilling/Controller/ProductContentController.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * HugaShop - Sell anything
+ *
+ * @author Andri Huga
+ * @version 1.0
+ *
+ */
+
+namespace HugaShop\Addons\ProductFilling\Controller;
+
+use HugaShop\Services\Design;
+use HugaShop\Services\Secure;
+use App\Services\LanguageService;
+use App\Controller\BaseAdminController;
+use HugaShop\Addons\BaseAddonTrait;
+use Symfony\Component\Routing\Attribute\Route;
+use HugaShop\Addons\ProductFilling\Models\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ProductContentController extends BaseAdminController
+{
+    use BaseAddonTrait;
+
+    #[Route('/ProductFilling/content/{id}', name: 'AddonProductFillingContent', requirements: ['id' => '\\d+'], priority: 20)]
+    public function index(int $id): Response
+    {
+        $this->checkAdminAccess('product_content');
+
+        LanguageService::languageCatch();
+
+        if (!empty($product = Secure::getInputCheckEditAccess(Product::class, $id))) {
+            Design::setFlashMessage('update', Product::updateProduct($id, $product));
+            return $this->redirectToRouteLang('AddonProductFillingContent', ['id' => $id]);
+        }
+
+        $product = Product::getOneEditTranslate($id);
+        if (empty($product->id)) {
+            return $this->redirectToRoute('AddonProductFilling');
+        }
+
+        Design::assign('product', $product);
+
+        return $this->fetchAddonResponse('product_content.tpl');
+    }
+}
+
