93fe9c0c4b6585e1d3c05b5b9076b353563399c0
Andri Huga <aa.guzhva@gmail.com>
2025-09-15 16:50:13 -0600

    ///

--- hugashop/crm/Addons/CronAgent/Services/CronAgentService.php
+++ hugashop/crm/Addons/CronAgent/Services/CronAgentService.php
@@ -10,6 +10,7 @@
 
 namespace HugaShop\Addons\CronAgent\Services;
 
+use HugaShop\Services\Cache;
 use HugaShop\Addons\CronAgent\Models\CronAgent as Agent;
 
 final class CronAgentService
@@ -20,10 +21,10 @@ final class CronAgentService
      */
     public static function run(): void
     {
-        $now = date('Y-m-d H:i:s');
+
         $agents = Agent::getList(['enabled' => 1], cache: null);
         foreach ($agents as $agent) {
-            if (self::isDue($agent, $now)) {
+            if (self::isDue($agent)) {
                 self::execute($agent);
             }
         }
@@ -35,12 +36,13 @@ final class CronAgentService
      * @param object $agent
      * @param string $now
      */
-    private static function isDue(object $agent, string $now): bool
+    private static function isDue(object $agent): bool
     {
         if (empty($agent->start_at)) {
             return false;
         }
 
+        $now = date('Y-m-d H:i:s');
         if (empty($agent->last_run_at)) {
             return $now >= $agent->start_at;
         }
@@ -49,6 +51,7 @@ final class CronAgentService
             'Y-m-d H:i:s',
             strtotime($agent->last_run_at . " +{$agent->period_hours} hours +{$agent->period_minutes} minutes")
         );
+
         return $now >= $next;
     }
 
@@ -62,6 +65,7 @@ final class CronAgentService
         $callable = str_replace('()', '', (string) $agent->function);
         if (str_contains($callable, '::')) {
             [$class, $method] = explode('::', $callable);
+            dump([$class, $method]);
             if (class_exists($class) && method_exists($class, $method)) {
                 try {
                     $class::$method();
@@ -72,5 +76,6 @@ final class CronAgentService
         }
 
         Agent::updateOne($agent->id, ['last_run_at' => date('Y-m-d H:i:s')]);
+        Cache::cache(Agent::class)->clear();
     }
 }
